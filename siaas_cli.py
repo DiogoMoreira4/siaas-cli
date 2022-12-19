5027fbe794c3f974fb6f7d611708566a0ccc9e55
João Pedro Seara <joao.pedro.seara@gmail.com>
2022-12-19 19:31:11 +0000

    API entrance point, version, env

--- siaas_cli.py
+++ siaas_cli.py
@@ -1,31 +1,53 @@
 import click
+import requests
+import urllib3
+import pprint
+import logging
+import os
 
+logger = logging.getLogger(__name__)
+try:
+   log_level=eval("logging."+os.getenv('SIAAS_LOG_LEVEL').upper())
+except:
+   log_level=logging.WARN
+logging.basicConfig(level=log_level)
 
 @click.group()
-def apis():
-    """A CLI wrapper for the API of SIAAS."""
+@click.version_option(version="1.0.0")
+def siaas():
+    """A CLI wrapper for the SIAAS API."""
 
-
-@click.option('-t', '--title', help='Name of API (matches via substring - i.e. "at" would return "cat" and "atlas".')
-@click.option('-c', '--category', help='Return only APIs from this category')
-@click.option('-a', '--no-auth', is_flag=True, help='Filter out APIs with required auth')
-@apis.command()
-def entries(title: str, category: str, no_auth: bool):
-    """List all cataloged APIs."""
-
-
-@click.option('-t', '--title', help='Name of API (matches via substring - i.e. "at" would return "cat" and "atlas".')
-@click.option('-c', '--category', help='Return only APIs from this category')
-@click.option('-a', '--no-auth', is_flag=True, help='Filter out APIs with required auth')
-@apis.command()
-def random(title: str, category: str, no_auth: bool):
-    """Get a random API."""
-
-
-@apis.command()
-def categories():
-    """List all categories."""
+@click.option('-a', '--api', help="SIAAS API URI.", envvar='SIAAS_API_URI')
+@click.option('-u', '--user', help="SIAAS API user.", envvar='SIAAS_API_USER')
+@click.option('-p', '--password', help="SIAAS API password.", envvar='SIAAS_API_PWD')
+@click.option('-c', '--ca_bundle', help="SIAAS SSL CA bundle path.", envvar='SIAAS_API_SSL_CA_BUNDLE')
+@click.option('-i', '--insecure', is_flag=True, help="Don't verify SSL endpoint.", envvar='SIAAS_API_SSL_IGNORE_VERIFY')
+@siaas.command()
+def api(api: str, user: str, password: str, ca_bundle: str, insecure: bool):
+    """
+    Shows SIAAS API information.
+    """
+    urllib3.disable_warnings()
+    if insecure==True:
+       logger.warning("SSL verification is off! This might have security implications while connecting to the server API.")
+       verify=False
+    else:
+       if len(ca_bundle or '')>0:
+         verify=ca_bundle
+       else:
+         verify=True
+    try:
+        r = requests.get(api+"/", timeout=60, verify=verify, allow_redirects=True, auth=(user,password))
+    except Exception as e:
+        logger.error("Error while performing a GET request to the server API: "+str(e))
+        return False
+    if r.status_code == 200:
+        logger.debug("All data that was read from the server API:\n" +
+                     pprint.pformat(r.json()))
+        print(pprint.pformat(r.json()))
+    else:
+        logger.error("Error getting data from the server API: "+str(r.status_code))
 
 
 if __name__ == '__main__':
-    apis(prog_name='siaas-cli')
+    siaas(prog_name='siaas-cli')
