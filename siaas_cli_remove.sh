4c82539ec2f375d2c1e390c8a9b962e47595a626
João Pedro Seara <joao.pedro.seara@gmail.com>
2022-12-19 17:44:37 +0000

    Removal script

---
+++ siaas_cli_remove.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "`readlink -f ${BASH_SOURCE[0]}`" )" &> /dev/null && pwd )
+
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root or using sudo!"
+  exit 1
+fi
+
+cd ${SCRIPT_DIR}
+
+rm -f /usr/local/bin/siaas-cli
+
+echo -e "\nSIAAS CLI was removed from the system.\n"
