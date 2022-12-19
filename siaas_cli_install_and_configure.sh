a74c3c3dd5acb8442cc438384d7380cdc9d8a26a
João Pedro Seara <joao.pedro.seara@gmail.com>
2022-12-19 17:51:26 +0000

    Install script tweaks

--- siaas_cli_install_and_configure.sh
+++ siaas_cli_install_and_configure.sh
@@ -16,4 +16,4 @@ apt-get install -y python3 python3-pip python3-venv git
 ln -fs ${SCRIPT_DIR}/siaas_cli_run.sh /usr/local/bin/siaas-cli
 #ln -fs ${SCRIPT_DIR}/log /var/log/siaas-cli
 
-echo -e "\nSIAAS CLI is installed. To use it: ./siaas-cli.py\n"
+echo -e "\nSIAAS CLI is installed. To use it: siaas-cli\n"
